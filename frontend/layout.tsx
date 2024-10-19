4d925a104ecf365f9c292bf9b966c148c5c3bfe2
DESKTOP-T78J8K3\dc <dalibor.kos23@gmail.com>
2024-10-20 01:31:33 +0200

    add frontend layout.tsx

---
+++ frontend/layout.tsx
@@ -0,0 +1,8 @@
+<head>
+    <link rel="icon" type="image/png" href="/public/icons/favicon-48x48.png" sizes="48x48" />
+    <link rel="icon" type="image/svg+xml" href="/public/icons/favicon.svg" />
+    <link rel="shortcut icon" href="/public/icons/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/icons/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="DAW Projects Dashboard" />
+    <link rel="manifest" href="/public/icons/site.webmanifest" />
+</head>
